f28c8f442a68993a1b166e8e071c9e844bf25f14
Tofweod <junqiaochen@qq.com>
2026-03-13 00:36:45 +0800

    update on 2026-03-13 00:36:45

--- bspwm/.config/sxhkd/scripts/show_help.sh
+++ bspwm/.config/sxhkd/scripts/show_help.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' $HOME/.config/sxhkd/sxhkdrc |
-  column -t -s $'\t' | rofi -dmenu -i -markup-rows -no-show-icons -width 2000 -yoffset 40 -no-config
+  column -t -s $'\t' | rofi -dmenu -i -markup-rows -no-show-icons -no-config -yoffset 40 -l 25 -theme-str 'window { width: 1200px; }'
