c4e8904dd60de22e4efc6c8d48fad46a979b85ee
Tofweod <junqiaochen@qq.com>
2026-04-20 00:19:57 +0800

    update on 2026-04-20 00:19:57, add click feature in music-mpris.sh

--- bspwm/.config/polybar/colorblocks/scripts/music-mpris.sh
+++ bspwm/.config/polybar/colorblocks/scripts/music-mpris.sh
@@ -1,6 +1,33 @@
 #! /bin/bash
 
-artist=$(playerctl -p $1 metadata | grep -E 'artist' | awk '{for(i=3;i<NF;i++){printf "%s ",$i};printf "%s",$i}')
-title=$(playerctl -p $1 metadata | grep -E 'title' | awk '{for(i=3;i<NF;i++){printf "%s ",$i};printf "%s",$i}')
+player=""
+action=""
 
-echo "$title"-"$artist"
+usage() {
+  echo "music-mpris.sh -p player [-a previous/next/play-pause]"
+}
+
+metadata() {
+  artist=$(playerctl -p $1 metadata | grep -E 'artist' | awk '{for(i=3;i<NF;i++){printf "%s ",$i};printf "%s",$i}')
+  title=$(playerctl -p $1 metadata | grep -E 'title' | awk '{for(i=3;i<NF;i++){printf "%s ",$i};printf "%s",$i}')
+  echo "$title"-"$artist"
+}
+
+while getopts "p:a:" opt; do
+  case $opt in
+  p) player="$OPTARG" ;;
+  a) action="$OPTARG" ;;
+  *) usage ;;
+  esac
+done
+
+if [[ -z $player ]]; then
+  usage
+  exit 1
+fi
+
+if [[ -z $action ]]; then
+  metadata $player
+else
+  playerctl -p $player $action
+fi
